01d86a144e749295088395b0da60aa30f5ac13b5
spenzoar <spenzoar@protonmail.com>
2022-02-08 18:12:46 -0600

    cli send now defaults to help command if none given

--- bitcoin/cli_send.sh
+++ bitcoin/cli_send.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-echo $1
+#default to help command if none given
+cmd="help"
+if [ -z $1 ]
+then
+	echo $cmd 
+else
+	cmd=$1
+fi
 
-./bitcoin-22.0/bin/bitcoin-cli -rpcconnect=192.168.2.108 -rpcport=8332 -rpcuser=bitcoin -rpcpassword=1234 getblockcount
+./bitcoin-22.0/bin/bitcoin-cli -rpcconnect=192.168.2.108 -rpcport=8332 -rpcuser=bitcoin -rpcpassword=1234 $cmd 
