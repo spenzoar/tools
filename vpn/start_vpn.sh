203ccd9853475f7bfe834dc50210593f6106b357
spenzoar <spenzoar@protonmail.com>
2022-02-14 15:26:50 -0600

    fix error with web start

--- vpn/start_vpn.sh
+++ vpn/start_vpn.sh
@@ -4,4 +4,7 @@ protonvpn-cli connect US-IL#15
 
 start-stop-daemon --start --name deluged --startas /usr/bin/deluged
 
-start-stop-daemon --start --name deluge-web --startas /usr/bin/deluged-web
+#https://forum.deluge-torrent.org/viewtopic.php?t=55673
+#https://git.deluge-torrent.org/deluge/commit/?h=develop&id=d6c96d629183e8bab2167ef56457f994017e7c85
+#had to apply above fix to get deluge-web to start without error
+start-stop-daemon --start --name deluge-web --startas /usr/bin/deluge-web
