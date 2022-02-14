4eef9a56cd9c240da9e113ffe21f1f9da805a8bb
spenzoar <spenzoar@protonmail.com>
2022-02-14 12:31:30 -0600

    add deluge-web to start/stop

--- vpn/start_vpn.sh
+++ vpn/start_vpn.sh
@@ -3,3 +3,5 @@
 protonvpn-cli connect US-IL#15
 
 start-stop-daemon --start --name deluged --startas /usr/bin/deluged
+
+start-stop-daemon --start --name deluge-web --startas /usr/bin/deluged-web
--- vpn/stop_vpn.sh
+++ vpn/stop_vpn.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-start-stop_daemon --stop --name deluged
+start-stop-daemon --stop --name deluge-web
+
+start-stop-daemon --stop --name deluged
 
 protonvpn-cli disconnect
